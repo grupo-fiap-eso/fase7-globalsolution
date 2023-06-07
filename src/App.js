65b5649257c1e474bb47ac2632bd0529013165ae
Guilherme Quirino de Melo <guilhermequirino74@gmail.com>
2023-06-07 16:53:36 -0300

    mover footer para o seu componente

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import Dados from './components/Dados'
 import Solucao from './components/Solucao'
 import DadosDaFome from './components/DadosDaFome'
 import FaleConosco from './components/FaleConosco'
+import Footer from './components/Footer'
 
 function App(){
   return(
     <BrowserRouter>
       <Routes>
-        
-        <Route path="/home" element={[<Menu/>, <Home/>]}/>
-        <Route path="/posts/:id" element={[<Menu/>, <PostPage/>]}/>
-        <Route path="/dados-da-fome" element={[<Menu/>, <DadosDaFome/>]}/>
-        <Route path="/inseguranca_alimentar" element={[<Menu/>, <Dados/>]}/>
-        <Route path="/nossa_solucao" element={[<Menu/>, <Solucao/>]}/>
-        <Route path="/fale-conosco" element={[<Menu/>, <FaleConosco/>]}/>
+        <Route path="/" element={[<Menu/>, <Home/>, <Footer/>]}/>
+        <Route path="/home" element={[<Menu/>, <Home/>, <Footer/>]}/>
+        <Route path="/posts/:id" element={[<Menu/>, <PostPage/>, <Footer/>]}/>
+        <Route path="/dados-da-fome" element={[<Menu/>, <DadosDaFome/>, <Footer/>]}/>
+        <Route path="/inseguranca_alimentar" element={[<Menu/>, <Dados/>, <Footer/>]}/>
+        <Route path="/nossa_solucao" element={[<Menu/>, <Solucao/>, <Footer/>]}/>
+        <Route path="/fale-conosco" element={[<Menu/>, <FaleConosco/>, <Footer/>]}/>
       </Routes>
     </BrowserRouter>
   )
