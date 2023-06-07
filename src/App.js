bff6e5be1486c5d70d2ca10ade933093d8031012
Ayla <84017026+Ayla-Carolina@users.noreply.github.com>
2023-06-07 15:47:57 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./components/Home"
 import Menu from "./components/Menu"
-
+import PostPage from './components/PostPage'
+import Dados from './components/Dados'
+import Solucao from './components/Solucao'
 
 function App(){
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={[<Menu/>, <Home/>]}/>
+        
         <Route path="/home" element={[<Menu/>, <Home/>]}/>
+        <Route path="/posts/:id" element={[<Menu/>, <PostPage/>]}/>
+        <Route path="/inseguranca_alimentar" element={[<Menu/>, <Dados/>]}/>
+        <Route path="/nossa_solucao" element={[<Menu/>, <Solucao/>]}/>
       </Routes>
     </BrowserRouter>
   )
--- src/estiloGlobal.js
+++ src/estiloGlobal.js
@@ -9,8 +9,14 @@ const globalStyle = createGlobalStyle`
   }
 
   body{
-    background-color: #3b8d1814;
+    background-color: #FEFAE0;
   }
+
+  .navbar{
+    background-color: #283618;
+
+  }
+
   nav{
     min-height: 65px;
     align-items: center;
@@ -18,8 +24,118 @@ const globalStyle = createGlobalStyle`
     font-size: 12px;
   }
 
-  .navbar{
-    background-color: #3b8d18 
+  .primeira-secao{
+    margin-top: 1%;
+    margin-inline: 4em;
+    background-color: #3b8d184d;
+    box-sizing: border-box;
+    display: block;
+  }
+
+  .p-md-5{
+    padding: 3rem;
+  }
+
+  .rounded{
+    border-radius: 20px;
+  }
+
+  .p-4{
+    padding: 1.5rem;
+  }
+  .mb-4{
+    margin-bottom: 1.5rem;
+  }
+
+  .link1{
+    text-decoration: none;
+    color: #183909;
+    font-weight:400;
+  }
+
+  .segunda-secao{
+    margin-top: 100px;
+    margin-inline: 3em;
+    box-sizing: border-box;
+  }
+
+  .terceira-secao{
+    margin-top: 90px;
+    margin-inline: 3em;
+  }
+
+  button {
+    padding: 7px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .home-artigos{
+    padding: 0;
+    position: relative;
+  }
+
+  .row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .card {
+    border-radius: 5px;
+    box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22);
+    padding: 30px;
+    margin: 20px;
+    width: 300px;
+    transition: all 0.3s ease-out;
+  }
+
+  .card:hover {
+    transform: translateY(-5px);
+    cursor: pointer;
+  }
+   
+  .card p {
+    color: #a3a5ae;
+    font-size: 16px;
+  }
+   
+  .image {
+    float: right;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .green {
+    border-left: 3px solid #3bb54a;
+  }
+
+  .home-sobre-nos{
+    margin-top: 8%;
+    margin-inline: 4em;
+    box-sizing: border-box;
+    display: block;
+  }
+
+  div .comunidade{
+    margin-top: 5%;
+    background-color: #dfecf84d;
+    border-radius: 20px;
+    padding-bottom: 60px;
+  }
+
+
+  div .comentarios{
+    width: 70%;
+    padding: 20px;
+    background-color: #fffffff9;
+    border: 2px solid #aaabab57;
+    margin-left: 16%;
+  }
+  .titulo-comunidade{
+    margin: 5%;
+    color: #2d2c2cef;
   }
 
   footer{
